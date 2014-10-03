44e8b1a0ea548f04e3b5f18398f49848d3e7aa88
shikajiro <shikajiro@gmail.com>
2014-10-03 10:43:28 +0900

    undebug

--- server.py
+++ server.py
@@ -39,4 +39,5 @@ def handle_invalid_usage(error):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    # app.run(debug=True, host='0.0.0.0')
+    app.run()
