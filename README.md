b05a553bbef76f4bdb07a38e8609993296c9e729
shikajiro <shikajiro@gmail.com>
2014-10-03 12:01:08 +0900

    使いやすくした

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-error_responser
+error responser
 ===============
 
 指定したエラーを返すサーバー。エラーのレスポンステストなどに。
+python、Flaskを利用。herokuにて稼働中。
+
+## design
+### URL
+http://error-responser.herokuapp.com/{status_code}/{error_code}
+### status_code
+httpのstatus codeを指定する
+### error_code
+status code だけでなく、エラー情報が詰まったjsonも必要な場合利用する。
+
+## sample
+http://error-responser.herokuapp.com/404/1
+
+```json
+{
+  "error": {
+    "code": 1,
+    "message": "this is error message"
+  }
+}
+```
--- server.py
+++ server.py
@@ -4,12 +4,10 @@ app = Flask(__name__)
 class InvalidUsage(Exception):
     status_code = 400
 
-    def __init__(self, msg_code, status_code=None, payload=None):
+    def __init__(self, error_code, status_code=None, payload=None):
         Exception.__init__(self)
-        if msg_code in {1, 2, 10, 11, 12, 13, 14}:
-            self.msg_code = msg_code
-        else:
-            self.msg_code = 0
+        if error_code in {0, 1, 2, 10, 11, 12, 13, 14}:
+            self.error_code = error_code
         if status_code is not None:
             self.status_code = status_code
         self.payload = payload
@@ -17,7 +15,7 @@ class InvalidUsage(Exception):
     def to_json(self):
         error_json = {
               'error':{
-                    'code':self.msg_code,
+                    'code':self.error_code,
                     'message':"this is error message"
               }
         }
@@ -25,12 +23,17 @@ class InvalidUsage(Exception):
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello Error Responser!'
 
-@app.route('/<int:error_code>/<int:msg_code>')
-def error_response(error_code, msg_code):
-    print "set error code %d" % (error_code)
-    raise InvalidUsage(msg_code=msg_code, status_code=error_code)
+@app.route('/<int:status_code>')
+def error_response(status_code):
+    print "set status code %d" % (status_code)
+    abort(status_code)
+
+@app.route('/<int:status_code>/<int:error_code>')
+def error_response_and_error_code(status_code, error_code):
+    print "set status code %d, error code %d" % (status_code, error_code)
+    raise InvalidUsage(error_code=error_code, status_code=status_code)
 
 @app.errorhandler(InvalidUsage)
 def handle_invalid_usage(error):
@@ -39,5 +42,5 @@ def handle_invalid_usage(error):
     return response
 
 if __name__ == '__main__':
-    # app.run(debug=True, host='0.0.0.0')
-    app.run()
+    app.run(debug=True, host='0.0.0.0')
+    # app.run()
